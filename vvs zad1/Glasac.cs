dc76cec9cddcfca282e989377aa0b2361eb68a0e
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2022-11-01 01:49:51 +0100

    basic interfejs

---
+++ vvs zad1/Glasac.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vvs_zad1
+{
+    internal class Glasac
+    {
+        bool glasao;
+        string ime, prezime, adresa,jik,datumRodjenja;
+        public Glasac(string ime, string prezime, string adresa, string datumRodjenja,bool glasao)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.adresa = adresa;
+            this.datumRodjenja = datumRodjenja;
+            this.jik = ime.Substring(0,2)+prezime.Substring(0,2)+adresa.Substring(0,2)+datumRodjenja.Substring(0,2);
+            this.glasao = glasao;
+        }
+        public string getJik()
+        {
+            return jik;
+        }
+        public bool getGlasao()
+        {
+            return glasao;
+        }
+    
+    }
+}
--- vvs zad1/Program.cs
+++ vvs zad1/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace vvs_zad1
 {
     internal class Program
     {
+        static List<Glasac> glasaci = new List<Glasac>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            glasaci.Add(new Glasac("Cigarettes", "After", "Keks", "Sweet", false));
+
+            while (true)
+            {
+                Console.WriteLine("Ako želite glasati unesite 1");
+                Console.WriteLine("Ako želite vidjeti statistiku unesite 2");
+                Console.WriteLine("Ako želite izaci iz app unesite 3");
+                int k = Convert.ToInt32(Console.ReadLine());
+                if (k == 1)
+                {
+                    Console.WriteLine("Unesite Vaš jedinstveni identifikacioni kod: ");
+                    string s=Console.ReadLine();
+                    int t = glasaci.FindIndex(gl => gl.getJik()==s );
+                    if(t==-1)
+                    {
+                        Console.WriteLine("Niste registrirani u ovom biračkom mjestu");
+                        continue;
+                    }
+                    else
+                    {
+                        if (!glasaci[t].getGlasao())
+                        {
+                            //glasaj
+                        }
+                        else
+                        {
+                            Console.WriteLine("Već ste glasali!");
+                            continue;
+                        }
+                    }
+                }
+                else if (k == 2)
+                {
+
+                }
+                else if (k == 3)
+                {
+                    break;
+                }
+                else
+                    continue;
+            }
         }
     }
 }
