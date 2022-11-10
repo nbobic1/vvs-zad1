2b2772ca3a17fa1d0c9044d3497ab6a58f649f85
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2022-11-10 23:21:46 +0100

    Podatci i dodjela mandata

--- vvs zad1/Program.cs
+++ vvs zad1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography.X509Certificates;
 
 namespace vvs_zad1
@@ -8,9 +9,15 @@ namespace vvs_zad1
     internal class Program
     {
         static List<Glasac> glasaci = new List<Glasac>();
+        static List<Kandidat> pobjedniciK = new List<Kandidat>();
         static List<Kandidat> sda = new List<Kandidat>();
+        static List<Kandidat> sdp = new List<Kandidat>();
+        static List<Kandidat> hdz = new List<Kandidat>();
+        static List<Kandidat> asda = new List<Kandidat>();
+        static List<Kandidat> pomak = new List<Kandidat>();
         static List<Kandidat> nezavisni=new List<Kandidat>();
-        static List<Tuple<List<Kandidat>,string>> stranke = new List<Tuple<List<Kandidat>,string>>();  
+        static List<Tuple<List<Kandidat>,string>> stranke = new List<Tuple<List<Kandidat>,string>>();
+        static List<Tuple<List<Kandidat>, string>> pobjedniciS = new List<Tuple<List<Kandidat>, string>>();
         public static int brojGlasova()
         {
             return glasaci.FindAll(x=>x.getGlasao()).Count;
@@ -35,6 +42,27 @@ namespace vvs_zad1
                 stat.Add(new Tuple<string, int>(stranke[i].Item2, (z*100)/g)); 
             }
             return stat;
+        }
+        public static void pobjednici()
+        {
+            int g = brojGlasova();
+            for (int i = 0; i < stranke.Count; i++)
+            {
+                List<Kandidat> t = stranke[i].Item1;
+                string s = stranke[i].Item2;
+                int z = 0;
+                for (int j = 0; j < t.Count; j++)
+                    z += t[j].brojG();
+                for (int j = 0; j < t.Count; j++)
+                {
+                    string ert = s + "-" + t[j].getIme() + " " + t[j].getPrezime();
+                    if (t[j].brojG() / z > 0.2)
+                        pobjedniciK.Add(t[j]);
+                  
+                }
+                if (z / g > 0.02)
+                    pobjedniciS.Add(stranke[i]);   
+            }
         }
         public static void statistikaIspis()
         {   
@@ -68,13 +96,71 @@ namespace vvs_zad1
             Console.WriteLine("Ukupna izlaznost: " + ((brojGlasova()*100) / glasaci.Count));
            
         }
+        public static void pod()
+        {
+
+
+            glasaci.Add(new Glasac("Fatih", "Fatić", "Tešanjska 12", "02.01.1998.", false));
+            glasaci.Add(new Glasac("Mujo", "Mujić", "Zmaja od Bosne 14", "05.01.1995.", false));
+            glasaci.Add(new Glasac("Huso", "Husić", "Novo Sarajevo", "14.02.1994.", false));
+            glasaci.Add(new Glasac("Pero", "Perić", "Čengić Vila", "02.04.1998.", false));
+            glasaci.Add(new Glasac("Hasnija", "Hasnić", "Stup", "02.01.2000.", false));
+            glasaci.Add(new Glasac("Esmeralda", "Kolašinac", "Fedhima bb", "15.05.1980.", false));
+            glasaci.Add(new Glasac("Lebron", "James", "Ozimice 2", "14.01.1994.", false));
+            glasaci.Add(new Glasac("Meho", "Mehić", "Grbavica", "02.01.1998.", false));
+            glasaci.Add(new Glasac("Benjamin", "Fehić", "Pofalići", "02.01.1998.", false));
+            glasaci.Add(new Glasac("Vučko", "Vučkić", "Titova", "21.12.2001.", false));
+            glasaci.Add(new Glasac("Meho", "Puzić", "Odžak", "02.01.1937.", false));
+            glasaci.Add(new Glasac("Mahir", "Fatić", "Tešanjska 12", "02.06.2001.", false));
+
+
+
+            sda.Add(new Kandidat("Hasnija", "Bulić"));
+            sda.Add(new Kandidat("Aki", "Akić"));
+            sda.Add(new Kandidat("Musa", "Nurkić"));
+            sda.Add(new Kandidat("Edin", "Atić"));
+            sda.Add(new Kandidat("Sulejman", "Halilovič"));
+
+            sdp.Add(new Kandidat("Munja", "Munjić"));
+            sdp.Add(new Kandidat("Amar", "Gegić"));
+            sdp.Add(new Kandidat("Kenan", "Kamenjaš"));
+            sdp.Add(new Kandidat("Edvin", "Kljaljić"));
+            sdp.Add(new Kandidat("Esad", "Plavi"));
+
+            hdz.Add(new Kandidat("Elon", "Musk"));
+            hdz.Add(new Kandidat("Rakan", "Mukič"));
+            hdz.Add(new Kandidat("Ela", "Makedonac"));
+            hdz.Add(new Kandidat("Ilija", "Nugato"));
+            hdz.Add(new Kandidat("Kakao", "Kamenjaš"));
+
+            asda.Add(new Kandidat("Fatka", "Fatkić"));
+            asda.Add(new Kandidat("Frugić", "Drugar"));
+            asda.Add(new Kandidat("Gospodar", "Fatkić"));
+            asda.Add(new Kandidat("Bilbo", "Bagins"));
+            asda.Add(new Kandidat("Edin", "Džeko"));
+
+            pomak.Add(new Kandidat("Romeo", "Lukaku"));
+            pomak.Add(new Kandidat("Cristiano", "Ronaldo"));
+            pomak.Add(new Kandidat("Tejt", "MegregoAr"));
+            pomak.Add(new Kandidat("Kanie", "West"));
+            pomak.Add(new Kandidat("Messi", "Ronaldo"));
+
+            nezavisni.Add(new Kandidat("Justin", "So"));
+            nezavisni.Add(new Kandidat("Angelina", "Boli"));
+            nezavisni.Add(new Kandidat("Marija", "Ana"));
+            nezavisni.Add(new Kandidat("Bull", "Pit"));
+            nezavisni.Add(new Kandidat("Vojage", "Breskvica"));
+            stranke.Add(new Tuple<List<Kandidat>, string>(sda, "sda"));
+            stranke.Add(new Tuple<List<Kandidat>, string>(nezavisni, "nezavisni"));
+            stranke.Add(new Tuple<List<Kandidat>, string>(pomak, "Pomak"));
+            stranke.Add(new Tuple<List<Kandidat>, string>(sdp, "Sdp"));
+            stranke.Add(new Tuple<List<Kandidat>, string>(asda, "asda"));
+            stranke.Add(new Tuple<List<Kandidat>, string>(hdz, "hdz"));
+
+        }
         static void Main(string[] args)
         {
-            glasaci.Add(new Glasac("Cigarettes", "After", "Keks", "Sweet", false));
-            sda.Add(new Kandidat("brm", "brm"));
-            nezavisni.Add(new Kandidat("triko", "sako"));
-            stranke.Add(new Tuple<List<Kandidat>,string>(sda,"sda"));
-            stranke.Add(new Tuple<List<Kandidat>, string>(nezavisni, "nezavisni"));
+            pod();
             while (true)
             {
                 Console.WriteLine("Ako želite glasati unesite 1");
@@ -128,6 +214,10 @@ namespace vvs_zad1
                                 stranke[stranke.Count - 1].Item1[zu - stranke.Count + 1].dodaj_glas();
                                 glasaci[t].setGlasao(true);
                                 Console.WriteLine("Uspjesno ste glasali!");
+                              if(glasaci.Count==brojGlasova())
+                                {
+                                    pobjednici();
+                                }
                                 continue;
                             }
                             else
@@ -158,6 +248,7 @@ namespace vvs_zad1
                                     Console.WriteLine("Unesite broj pored kandidata kako bi glasali za istog, a -1 za izlaz");
                                 }
                                 glasaci[t].setGlasao(true);
+                                pobjednici();
                                 Console.WriteLine("Uspjesno ste galsali za kandidate stranke: " + stranke[zu].Item2);
                             }
                         }
